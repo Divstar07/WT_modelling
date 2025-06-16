9ebec7fd174d30257a502880699ee5aff9369bf1
Divstar07 <126425570+Divstar07@users.noreply.github.com>
2025-06-16 14:42:09 -0400

    Add files via upload

--- wt_modelling.m
+++ wt_modelling.m
@@ -2,7 +2,7 @@
 % input parameters
 
 % Setup input parameters
-TSR = 6.0; % tip speed ratio (non-dimensional)
+TSR = 4.5; % tip speed ratio (non-dimensional)
 P = 4.0; % desired power in Watts
 rho_air = 1.225; % density of air in kg/m^3
 c_p = 0.4; % estimated coefficient of power (non-dimensional)
@@ -10,5 +10,5 @@ sys_eff = 0.7; % efficiency of generator and gearbox system
 u_wind = 14; % wind velocity in m/s
 
 % Calculate expected radius and rotational speed
-R = 2*P/(u^3*rho_air*pi*c_p*sys_eff) % expected radius of turbine rotor
-omega_rotor = (TSR*u_wind)/R % rotational speed of the rotor
+R = sqrt(2*P/(u_wind^3*rho_air*pi*c_p*sys_eff)) % expected radius of turbine rotor in meters
+omega_rotor = ((TSR*u_wind)/R) * (60/(2*pi)) % rotational speed of the rotor in rev/min

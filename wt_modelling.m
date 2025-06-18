e8e41a854a0a5f79e307ba3c2f0d9efae5d1e9a9
Divstar07 <126425570+Divstar07@users.noreply.github.com>
2025-06-18 15:05:41 -0400

    Add PWM functionality, and modelling of thrust

--- wt_modelling.m
+++ wt_modelling.m
@@ -1,4 +1,7 @@
-% Model expected geometry and rotational speed of a wind turbine given
+clc; clearvars;
+
+%% Rotor design
+% Use this section if modelling expected geometry and rotational speed of a wind turbine given
 % input parameters
 
 % Setup input parameters
@@ -6,9 +9,43 @@ TSR = 4.5; % tip speed ratio (non-dimensional)
 P = 4.0; % desired power in Watts
 rho_air = 1.225; % density of air in kg/m^3
 c_p = 0.4; % estimated coefficient of power (non-dimensional)
+c_T = 0.8; % estimated thrust coefficient (non-dimensional)
 sys_eff = 0.7; % efficiency of generator and gearbox system
-u_wind = 14; % wind velocity in m/s
+fan_PWM = 50; % PWM of fan array in percent
 
-% Calculate expected radius and rotational speed
-R = sqrt(2*P/(u_wind^3*rho_air*pi*c_p*sys_eff)) % expected radius of turbine rotor in meters
-omega_rotor = ((TSR*u_wind)/R) * (60/(2*pi)) % rotational speed of the rotor in rev/min
+
+% Intermediate results
+q = 0.5*rho_air*(u_wind^2);
+u_wind = 0.1925*fan_PWM - 0.4635 % fan speed without contraction
+
+% Calculate expected rotor diameter and rotational speed
+D = 2*(sqrt(2*P/(u_wind^3*rho_air*pi*c_p*sys_eff))) % expected radius of turbine rotor in meters
+omega_rotor = ((TSR*u_wind)/(D/2)) * (60/(2*pi)) % rotational speed of the rotor in rev/min
+A = (pi*D^2)/4; % Area of rotor
+T = q*A*c_T % Thrust force in Newtons
+
+
+% =========================================================================================================================
+% =========================================================================================================================
+
+
+%% Rotor evaluation
+% Use this section if predicting forces given a turbine design
+% input parameters, modelled after WiRE-01
+clc; clearvars;
+
+% Setup input parameters
+TSR = 4.5; % tip speed ratio (non-dimensional)
+rho_air = 1.225; % density of air in kg/m^3
+c_p = 0.4; % estimated coefficient of power (non-dimensional)
+c_T = 0.8; % estimated thrust coefficient (non-dimensional)
+fan_PWM = 10; % PWM of fan array in percent
+D = 0.15; % diameter of turbine rotor in meters
+
+% Intermediate results
+u_wind = 0.1925*fan_PWM - 0.4635 % fan speed without contraction
+q = 0.5*rho_air*(u_wind^2);
+
+% Calculate expected rotor diameter and rotational speed
+A = (pi*D^2)/4; % Area of rotor in m^2
+T = q*A*c_T % Thrust force in Newtons
